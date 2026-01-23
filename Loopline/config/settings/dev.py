b401c0483c70a1f9faf8545fc5cb4bc02409b3c1
anup-wade <anup.wade@outlook.com>
2026-01-23 12:20:00 +0530

    Add stage environment configs and Docker/Nginx setup

--- Loopline/config/settings/dev.py
+++ Loopline/config/settings/dev.py
@@ -1,45 +1,42 @@
 from .base import *
 import os
 
+# --------------------
+# BASIC DEV SETTINGS
+# --------------------
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "34.30.48.239",
-]
+ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = [
-    "http://34.30.48.239",
-]
+# --------------------
+# DATABASE (Docker)
+# --------------------
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "loopline",
-        "USER": "loopline_user",
-        "PASSWORD": "loopline_pass",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST", "db"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
 
-
-
-FRONTEND_URL = os.getenv("FRONTEND_URL")
+# --------------------
+# CORS / CSRF
+# --------------------
+FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:5173")
 
 CORS_ALLOWED_ORIGINS = [FRONTEND_URL]
 CSRF_TRUSTED_ORIGINS = [FRONTEND_URL]
 
+# --------------------
+# DEV TOOLS
+# --------------------
 INSTALLED_APPS += ["e2e_test_utils"]
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-
+# --------------------
+# EMAIL (DEV SAFE)
+# --------------------
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+DEFAULT_FROM_EMAIL = "dev@localhost"
